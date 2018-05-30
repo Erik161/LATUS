d402feb633a2f44a9bd4864a6f377ec194bd6950
Erik Hernandez <erikestuardo_161@hotmail.com>
2018-05-30 04:34:16 -0600

    trayectoria story, logo, fuction firebase

--- js/firebasecontact.js
+++ js/firebasecontact.js
@@ -1,9 +1,46 @@
-//listen for form submit
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDn-EHla66uWMEDTiviv05tep_0Z9FHjDA",
+    authDomain: "contact-form-latus.firebaseapp.com",
+    databaseURL: "https://contact-form-latus.firebaseio.com",
+    projectId: "contact-form-latus",
+    storageBucket: "contact-form-latus.appspot.com",
+    messagingSenderId: "485296532032"
+  };
+  firebase.initializeApp(config);
 
-document.getElementById('contactForm').addEventListener('submit', submitform);
+  // Reference messages collection
+var messagesRef = firebase.database().ref('messages');
 
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
 
-function submitform(e){
-    e.preventDefault();
+// Submit form
+function submitForm(e){
+  e.preventDefault();
 
+    //Get Values
+    var name = getInputval('name');
+    var email = getInputval('email');
+    var phone = getInputval('phone');
+    var message = getInputval('message');
+
+    //save Message 
+   saveMessage(name, email, phone, message);
 }
+
+// Function to get get form values
+function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+
+// Save message to firebase
+function saveMessage(name, email, phone, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      email:email,
+      phone:phone,
+      message:message
+    });
+  }
