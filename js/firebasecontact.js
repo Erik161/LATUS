8d47d1ca0db2d3154185f52fb2e55a7e8d144a42
Erik Hernandez <erikestuardo_161@hotmail.com>
2018-06-25 10:01:03 -0600

    Update firebasecontact.js
    Old delete code, add Code New

--- js/firebasecontact.js
+++ js/firebasecontact.js
@@ -2,47 +2,36 @@
   // Reference messages collection
 var messagesRef = firebase.database().ref('messages');
 
-// Listen for form submit
-document.getElementById('contactForm').addEventListener('submit', submitForm);
+//MODIFICACION DE TODO EL CODIGO
+document.getElementById('contactForm'); // Obtenemos las referencia al formulario
 
-// Submit form
-function submitForm(e){
-  e.preventDefault();
-
-    //Get Values
-    var name = getInputval('name');
-    var email = getInputval('email');
-    var phone = getInputval('phone');
-    var message = getInputval('message');
-
-    //save Message 
-   saveMessage(name, email, phone, message);
+if(form){//si existe nuestro elemento en memoria este se quedara escuchando al evento submit del formulario
+ form.addEventListener('submit', contactForm); //Al momento de enviar el formulario, ejecuta la función "contactForm"
 }
 
+fuction contactForm(event){
+  event.preventDefault(); //Prevenimos el comportamiento por defecto de un formulario (Enviar por URL los parametros) 
+  
+  const name =  document.getElementById('name');//Obtenemos la referencia  a cada uno de nuestros elementos del formulario
+  const email = document.getElementById('email');
+  const phone = document.getElementById('phone');
+  const message = document.getElementById('message');
+  const data = {
+  'name': name.value,
+  'email': email.value,
+  'phone': phone.value,
+  'message': message.value  
+  }; //Creamos un objecto con todos los elementos de nuestro formulario.
+  saveContactForm(data); //Enviamos la informacion obtenida por el usuario a la funcion que encarga de guarfar la informacion en firebase
+  form.reset(); //borramos todos los campos.
+}
 
-  // Show alert
-  document.querySelector('.alert').style.display = 'block';
-
-  // Hide alert after 3 seconds
-  setTimeout(function(){
-    document.querySelector('.alert').style.display = 'none';
-  },3000);
-  // Clear form
-  document.getElementById('contactForm').reset();
-
-
-// Function to get get form values
-function getInputVal(id){
-    return document.getElementById(id).value;
-  }
-
-// Save message to firebase
-function saveMessage(name, email, phone, message){
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-      name: name,
-      email:email,
-      phone:phone,
-      message:message
-    });
-  }
+fuction saveContactForm(data){
+  firebase.database().ref('contact').push(data)//Hacemos referencia al metodo database de el SDK y hacemos referencia el nombre del objeto que contendra nuestros registros y empujamos los nuevos envios de datos.
+  .then(fuction(){
+        alert('Mensaje Guardado');//Si la peticion es correcta y almaceno los datos mostramos un mensaje al usuario.  
+        })
+  .catch(fuction(){
+        alert('Mensaje No Guardado'); //En caso de ocurrir un error le mostramos al usuario que ocurrio un error.
+        });      
+};
